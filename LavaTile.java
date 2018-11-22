ad8cc5dacbae4c49906044f0ccccab13ef1f42a3
Redouan <302678383@student.rocmondriaan.nl>
2018-11-22 15:59:15 +0100

    LavaTile added

--- LavaTile.java
+++ LavaTile.java
@@ -8,12 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class LavaTile extends Tile
 {
-    /**
-     * Act - do whatever the LavaTile wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public LavaTile(String image, int width, int height) 
-    {
-        super(image, width, height);
+    public LavaTile(String image, int width, int heigth) {
+        super(image, width, heigth);
     }    
 }
